664fadfad4b0d9dd17fab24d7bb28e8bcaa9d386
IvanNajdovski <ivan_najdovski@hotmail.com>
2018-06-19 10:02:14 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,17 +2,9 @@
 
 		socket.on('connect', function () {
 			console.log('Connected on server');
-
-
-			socket.emit('createMessage', {
-				from: "dime",
-				text: "What up?"
-			});
 		});
 
-
-
-		socket.on('disconnect', function () {
+	  	socket.on('disconnect', function () {
 			console.log('Disconnected from server');
 		});
 
