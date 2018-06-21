406d3dfe8eb6d511d36345caf5855d4815997e68
IvanNajdovski <ivan_najdovski@hotmail.com>
2018-06-21 16:03:13 +0200

    update from diferent work station

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,13 +1,28 @@
 const expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
-	let from = 'Dime';
-	let text = 'Shu prais';
-	let message = generateMessage(from, text);
+		it('should generate correct message object', () => {
+		let from = 'Dime';
+		let text = 'Shu prais';
+		let message = generateMessage(from, text);
 
-	expect(message.from).toBe('Dime');
-	expect(message.text).toBe(text);
-	expect(typeof message.createdAt).toBe('number');
-    
+		expect(message.from).toBe('Dime');
+		expect(message.text).toBe(text);
+		expect(typeof message.createdAt).toBe('number');
+	}); 
+});
+
+describe('generateLocationMessage', () => {
+	it('should generate correct location object', () => {
+		let from ='Dime';
+		let latitude = 41.997346199999996;
+		let longitude = 21.4279956;
+		let message = generateLocationMessage(from, latitude, longitude)
+
+		expect (message.from).toBe('Dime');
+		expect (typeof message.createdAt).toBe('number');
+		expect (message.url).toBe(`https://www.google.com/maps/?q=${latitude},${longitude}`);
+
+	});
 });
